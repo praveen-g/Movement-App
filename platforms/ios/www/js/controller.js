9fecd5ad8972eec8b2eac3fb715f111e2d058c00
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-12-16 18:01:13 -0500

    a few changes

--- platforms/ios/www/js/controller.js
+++ platforms/ios/www/js/controller.js
@@ -1,29 +1,57 @@
-var app=angular.module('location',[]);
+var app=angular.module('location');
 app.controller('GeoCtrl', function($scope, $cordovaGeolocation) {
-      var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
+  $scope.points = [];
+
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
   $cordovaGeolocation
     .getCurrentPosition(posOptions)
     .then(function (position) {
-      $scope.latitude  = position.coords.latitude
-      $scope.longitude = position.coords.longitude
+      
+      var d = new Date(position.timestamp);
+      d=d.toTimeString();
+      
+      // add the initial lat / long
+      $scope.points.push({
+        "lat":position.coords.latitude, 
+        "long":position.coords.longitude, 
+        "time":d
+      });
+
     }, function(err) {
       // error
     });
-
-  /*
+  
   var watchOptions = {
-    timeout : 3000,
+    timeout : 1000,
     enableHighAccuracy: false // may cause errors if true
   };
 
   var watch = $cordovaGeolocation.watchPosition(watchOptions);
+  
   watch.then(
     null,
     function(err) {
       // error
     },
     function(position) {
-      var lat  = position.coords.latitude
-      var long = position.coords.longitude
+      
+      console.log("updating the position")
+
+      var d = new Date(position.timestamp);
+      d=d.toTimeString();
+
+      var point = {
+        "lat":position.coords.latitude, 
+        "long":position.coords.longitude, 
+        "time":d
+      };
+
+      $scope.points.push(point);
+      
+      
   });
-*/
+
+
+});
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,34 +1,56 @@
 var app=angular.module('location');
 app.controller('GeoCtrl', function($scope, $cordovaGeolocation) {
 
+  $scope.points = [];
+
   var posOptions = {timeout: 10000, enableHighAccuracy: false};
 
   $cordovaGeolocation
     .getCurrentPosition(posOptions)
     .then(function (position) {
+      
       var d = new Date(position.timestamp);
       d=d.toTimeString();
-      $scope.points = [{"lat":position.coords.latitude, "long":position.coords.longitude, "time":d}];
+      
+      // add the initial lat / long
+      $scope.points.push({
+        "lat":position.coords.latitude, 
+        "long":position.coords.longitude, 
+        "time":d
+      });
+
     }, function(err) {
       // error
     });
   
   var watchOptions = {
-    timeout : 3000,
+    timeout : 1000,
     enableHighAccuracy: false // may cause errors if true
   };
 
   var watch = $cordovaGeolocation.watchPosition(watchOptions);
+  
   watch.then(
     null,
     function(err) {
       // error
     },
     function(position) {
+      
+      console.log("updating the position")
+
       var d = new Date(position.timestamp);
       d=d.toTimeString();
-      point= { "lat":position.coords.latitude, "long":position.coords.longitude, "time":d};
+
+      var point = {
+        "lat":position.coords.latitude, 
+        "long":position.coords.longitude, 
+        "time":d
+      };
+
       $scope.points.push(point);
+      
+      
   });
 
 
